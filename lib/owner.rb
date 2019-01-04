690d800f3e051d37bd7fecbf449ffe6e433e5a09
Anysia V <anysiavalko@gmail.com>
2019-01-04 03:08:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -23,8 +23,17 @@ class Owner
   end
   
   def buy_fish(name)
-    pets[:fishes]<< Fish.new(name)
+    pets[:fishes] << Fish.new(name)
   end 
+  
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
+  
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+  
   
   
   def species 
